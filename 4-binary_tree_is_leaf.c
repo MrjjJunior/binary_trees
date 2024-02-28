1183059ccde7ae475a23f3f16ba451acb73a9abe
Tshepiso <tlhongtshepiso2@gmail.com>
2024-02-28 12:13:25 +0200

    Task 5 & 6

---
+++ 4-binary_tree_is_leaf.c
@@ -0,0 +1,8 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_is_leaf - Checks if a node is a leaf or not
+ * @node: A pointer to the node to check.
+ *
+ * Return: If the node is a leaf 1 else 0.
+ */
+
---
+++ 5-binary_tree_is_root.c
@@ -0,0 +1,14 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_is_root - Checks if a node is a root
+ * @node: A pointer to the node chexk.
+ *
+ * Return:  1 if node is root else 0
+ */
+int binary_tree_is_root(const binary_tree_t *node)
+{
+	if (node == NULL || node->parent != NULL)
+		return (0);
+
+	return (1);
+}
